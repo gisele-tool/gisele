e2acf21e94b8dc2fc0cd2179b3b493cc54fc5ab4
Bernard Lambeau <blambeau@gmail.com>
2012-02-16 10:14:36 +0100

    Add rule for boolean expressions.

--- spec/unit/language/test_grammar.rb
+++ spec/unit/language/test_grammar.rb
@@ -204,5 +204,37 @@ module Gisele::Language
 
     end # trackvar_def
 
+    describe 'the bool_expr rule' do
+
+      it 'parses single variable references' do
+        parse('diagKnown', :bool_expr).should eq('diagKnown')
+      end
+
+      it 'parses boolean literals' do
+        parse('true',  :bool_expr).should eq('true')
+        parse('false', :bool_expr).should eq('false')
+      end
+
+      it 'parses negated expression' do
+        parse('not diagKnown', :bool_expr).should eq('not diagKnown')
+        parse('not true', :bool_expr).should eq('not true')
+        parse('not false', :bool_expr).should eq('not false')
+      end
+
+      it 'parses or expressions' do
+        parse('diagKnown or platLow', :bool_expr).should eq('diagKnown or platLow')
+      end
+
+      it 'parses and expressions' do
+        parse('diagKnown and platLow', :bool_expr).should eq('diagKnown and platLow')
+      end
+
+      it 'parses complex expressions' do
+        expr = 'diagKnown and (platLow or not(metastased and mplus))'
+        parse(expr, :bool_expr).should eq(expr)
+      end
+
+    end # bool_expr
+
   end
 end
