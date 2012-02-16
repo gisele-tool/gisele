a8260dfad974a94232d9d42f0fc6f49a92ce2123
Bernard Lambeau <blambeau@gmail.com>
2012-02-16 09:27:06 +0100

    Add a rule for parsing event names.

--- spec/unit/language/test_grammar.rb
+++ spec/unit/language/test_grammar.rb
@@ -35,7 +35,22 @@ module Gisele::Language
         lambda{ parse('NotAVarName',  :variable_name) }.should raise_error(Citrus::ParseError)
       end
 
-    end # task_name
+    end # variable_name
+
+    describe 'the event_name rule' do
+
+      it 'parses correct event names' do
+        parse('a',          :event_name).should eq('a')
+        parse('event',      :event_name).should eq('event')
+        parse('event_name', :event_name).should eq('event_name')
+      end
+
+      it 'raises on invalid variable names' do
+        lambda{ parse('NotAnEventName',  :event_name) }.should raise_error(Citrus::ParseError)
+        lambda{ parse('notAnEventName',  :event_name) }.should raise_error(Citrus::ParseError)
+      end
+
+    end # event_name
 
   end
 end
