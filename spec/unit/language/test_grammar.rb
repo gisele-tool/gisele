efe6aabc038673b7e260caf9dc8ad08afbe6b6a9
Bernard Lambeau <blambeau@gmail.com>
2012-02-16 09:23:34 +0100

    Add rule for parsing variable names.

--- spec/unit/language/test_grammar.rb
+++ spec/unit/language/test_grammar.rb
@@ -23,5 +23,19 @@ module Gisele::Language
 
     end # task_name
 
+    describe 'the variable_name rule' do
+
+      it 'parses correct variable names' do
+        parse('a',         :variable_name).should eq('a')
+        parse('diagnosis', :variable_name).should eq('diagnosis')
+        parse('varName',   :variable_name).should eq('varName')
+      end
+
+      it 'raises on invalid variable names' do
+        lambda{ parse('NotAVarName',  :variable_name) }.should raise_error(Citrus::ParseError)
+      end
+
+    end # task_name
+
   end
 end
