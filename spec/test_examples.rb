d752a985722c648741d4b61cdb4ed784173436e6
Bernard Lambeau <blambeau@gmail.com>
2012-02-16 17:42:54 +0100

    Add examples.

---
+++ spec/test_examples.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+describe "The examples" do
+  
+  (Path.backfind(".[examples]")/:examples).glob("**/*.gis").each do |file|
+
+    describe file do
+      it 'parses without any error' do
+        Gisele::parse(file).should be_a(Array)
+      end
+    end
+
+  end
+  
+end
