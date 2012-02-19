cd49629122b88136503467470da070462389c469
Bernard Lambeau <blambeau@gmail.com>
2012-02-19 11:26:33 +0100

    Move SugarRemoval::IfToGuardedCommands as IfToCase

--- lib/gisele/language/sugar_removal.rb
+++ lib/gisele/language/sugar_removal.rb
@@ -4,72 +4,9 @@ module Gisele
       alias :on_missing :copy_and_applyall
 
       def on_if_st(node)
-        IfToGuardedCommands.new(self).call(node)
+        IfToCase.new(self).call(node)
       end
 
-      class IfToGuardedCommands < Rewriter
-        alias :on_missing :copy_and_applyall
-
-        def initialize(parent = nil)
-          @parent = parent || self
-        end
-
-        def on_if_st(node)
-          condition, dost, *clauses = node.children
-
-          # create case_st with same markers as the if_st
-          when_clause = [:when_clause, condition, @parent.call(dost)]
-          when_clause = node(when_clause, node.markers.dup)
-          base        = [:case_st, when_clause]
-          base        = node(base, node.markers.dup)
-
-          # this is the condition for elsif clauses
-          @condition = negate(condition.last)
-
-          # make injection now
-          clauses.inject base do |memo,clause|
-            memo << call(clause)
-          end
-        end
-
-        def on_elsif_clause(node)
-          condition, dost, = node.children
-
-          # install new conditions for me and next elsif clauses
-          condition = condition.last
-          previous  = @condition
-          @condition = [:bool_and, negate(condition), @condition]
-
-          # convert elsif to when and keep the markers
-          base = \
-            [:when_clause,
-              [:bool_expr, [:bool_and, condition, previous]],
-              @parent.call(dost) ]
-          node(base, node.markers.dup)
-        end
-
-        def on_else_clause(node)
-          dost, = node.children
-
-          # convert else to when and keep the markers
-          base = \
-            [:when_clause,
-              [:bool_expr, @condition],
-              @parent.call(dost)]
-          node(base, node.markers.dup)
-        end
-
-        private
-
-        def negate(cond)
-          if cond.rule_name == :bool_not
-            cond.last
-          else
-            [:bool_not, cond]
-          end
-        end
-
-      end # class IfToGuardedCommands
     end # class SugarRemoval
   end # module Language
 end # module Gisele
---
+++ spec/unit/language/test_if_to_case.rb
@@ -0,0 +1,108 @@
+require 'spec_helper'
+module Gisele::Language
+  describe IfToCase do
+    include AST::Helpers
+
+    def ast(source)
+      Syntax.ast(source.strip, :root => :if_st)
+    end
+
+    def rewrite(ast)
+      IfToCase.new.call(ast)
+    end
+
+    it 'rewrites single if correctly' do
+      source   = ast("if goodCond Task1 end")
+      expected = \
+        [:case_st,
+          [:when_clause,
+            [:bool_expr, [:var_ref, "goodCond"]],
+            [:task_call_st, "Task1"] ]]
+      rewrite(source).should eq(expected)
+    end
+
+    it 'negates the else clause' do
+      source   = ast("if goodCond Task1 else Task2 end")
+      expected = \
+        [:case_st,
+          [:when_clause,
+            [:bool_expr, [:var_ref, "goodCond"]],
+            [:task_call_st, "Task1"] ],
+          [:when_clause,
+            [:bool_expr, [:bool_not, [:var_ref, "goodCond"]]],
+            [:task_call_st, "Task2"] ]
+        ]
+      rewrite(source).should eq(expected)
+    end
+
+    it 'handles elsif clauses correctly' do
+      source = ast(<<-IF)
+        if    c1 Task1
+        elsif c2 Task2
+        elsif c3 Task3
+        else  Task4
+        end
+      IF
+      expected = \
+        [:case_st,
+          [:when_clause,
+            [:bool_expr, [:var_ref, "c1"]],
+            [:task_call_st, "Task1"] ],
+          [:when_clause,
+            [:bool_expr, [:bool_and,
+              [:var_ref, "c2"],
+              [:bool_not, [:var_ref, "c1"]] ]],
+            [:task_call_st, "Task2"] ],
+          [:when_clause,
+            [:bool_expr, [:bool_and,
+              [:var_ref, "c3"],
+              [:bool_and,
+                [:bool_not, [:var_ref, "c2"]],
+                [:bool_not, [:var_ref, "c1"]] ]]],
+            [:task_call_st, "Task3"] ],
+          [:when_clause,
+            [:bool_expr, [:bool_and,
+              [:bool_not, [:var_ref, "c3"]],
+              [:bool_and,
+                [:bool_not, [:var_ref, "c2"]],
+                [:bool_not, [:var_ref, "c1"]]]]],
+            [:task_call_st, "Task4"] ],
+        ]
+      rewrite(source).should eq(expected)
+    end
+
+    it 'avoids double negations' do
+      source   = ast("if not(goodCond) Task1 else Task2 end")
+      expected = \
+        [:case_st,
+          [:when_clause,
+            [:bool_expr, [:bool_not, [:var_ref, "goodCond"]]],
+            [:task_call_st, "Task1"] ],
+          [:when_clause,
+            [:bool_expr, [:var_ref, "goodCond"]],
+            [:task_call_st, "Task2"] ] ]
+      rewrite(source).should eq(expected)
+    end
+
+    it 'recurse on other nodes' do
+      if_st = ast("if goodCond Task1 end")
+      rw_st = rewrite(if_st)
+      rewrite([:unit, if_st]).should eq([:unit, rw_st])
+    end
+
+    it 'keeps traceability markers on a single if' do
+      if_st = ast("if goodCond Task1 end")
+      rw_st = rewrite(if_st)
+      rw_st.markers.should eq(if_st.markers)
+      rw_st.last.markers.should eq(if_st.markers)
+    end
+
+    it 'keeps traceability markers when a else if present' do
+      if_st = ast("if goodCond Task1 else Task2 end")
+      rw_st = rewrite(if_st)
+      rw_st.markers.should eq(if_st.markers)
+      rw_st.last.markers.should eq(if_st.last.markers)
+    end
+
+  end
+end
