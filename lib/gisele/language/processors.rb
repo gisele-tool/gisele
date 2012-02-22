4e8e02cbea3b7acaa74ec066135e3305e669838c
Bernard Lambeau <blambeau@gmail.com>
2012-02-22 19:19:13 +0100

    Resurect the scoping helper.

--- lib/gisele/language/processors.rb
+++ lib/gisele/language/processors.rb
@@ -1,4 +1,4 @@
-#require_relative 'processors/scoping_helper'
+require_relative 'processors/scoping_helper'
 require_relative 'processors/elsif_flattener'
 require_relative 'processors/if_to_case'
 require_relative 'processors/sugar_removal'
