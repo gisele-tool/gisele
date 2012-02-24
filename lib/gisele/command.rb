b8e86e4e5320630a7b478e616334690f949849be
Bernard Lambeau <blambeau@gmail.com>
2012-02-24 18:01:09 +0100

    The gisele command is a compiler in fact.

--- lib/gisele/command.rb
+++ lib/gisele/command.rb
@@ -31,18 +31,21 @@ module Gisele
 
     # Install options
     options do |opt|
-      @print_ast = nil
-      opt.on('--ast=[MODE]', 'Prints the process abstract syntax tree (debug,ruby)') do |value|
-        @print_ast = (value || "debug").to_sym
-      end
       @sugar = true
       opt.on('--no-sugar', 'Apply syntactic sugar removal') do
         @sugar = false
       end
-      @print_graph = nil
-      opt.on('--graph=[MODE]', 'Converts and print a graph (dot)') do |value|
-        @print_graph = (value || "dot").to_sym
+
+      @compile_mode = [:ast, "debug"]
+      opt.on('--ast=[MODE]',
+             'Compile as an abstract syntax tree (debug,ruby)') do |value|
+        @compile_mode = [:ast, (value || "debug").to_sym]
       end
+      opt.on('--graph=[MODE]',
+             'Compile as a workflow graph (dot)') do |value|
+        @compile_mode = [:graph, (value || "dot").to_sym]
+      end
+
       opt.on_tail('--help', "Show this help message") do
         raise Quickl::Help
       end
@@ -61,23 +64,19 @@ module Gisele
       ast = Gisele.ast(file)
       ast = Language::SugarRemoval.call(ast) unless @sugar
 
-      print_ast(ast, @print_ast) if @print_ast
-      print_graph(ast, @print_graph) if @print_graph
+      send :"compile_#{@compile_mode.first}", ast, *@compile_mode[1..-1]
     end
 
     private
 
-    def print_ast(ast, option)
+    def compile_ast(ast, option)
       require 'awesome_print'
-      options = case option
-                when :ruby then {index: false, plain: true}
-                else
-                  {}
-                end
+      options = {}
+      options = {index: false, plain: true} if option == :ruby
       ap ast, options
     end
 
-    def print_graph(ast, option)
+    def compile_graph(ast, option)
       graphs = Gisele::Compiling::ToGraph.call(ast)
       graphs.each do |graph|
         puts graph.to_dot
